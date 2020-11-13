f005fcf5f21f173ce39ff147854866f3b1e28c1c
EJ <ejcindunil@gmail.com>
2020-11-13 11:20:03 +0530

    add tablayout with ViewPager2

---
+++ TabView_ViewPager2/app/src/main/java/com/example/ej/tabview_viewpager2/CollectionDemoFragment.java
@@ -0,0 +1,49 @@
+package com.example.ej.tabview_viewpager2;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class CollectionDemoFragment extends Fragment {
+    DemoCollectionAdapter demoCollectionAdapter;
+    ViewPager2 viewPager;
+
+
+    public CollectionDemoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_collection_demo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        demoCollectionAdapter = new DemoCollectionAdapter(this);
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(demoCollectionAdapter);
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText("OBJECT " + (position + 1))
+        ).attach();
+    }
+}
---
+++ TabView_ViewPager2/app/src/main/java/com/example/ej/tabview_viewpager2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ej.tabview_viewpager2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(new CollectionDemoFragment(),"coll")
+                .commit();
+    }
+}
